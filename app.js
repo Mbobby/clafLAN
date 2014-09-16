e8f4a55c711a39001bf1ad39814b8a9f6c431d34
Emmanuel Mong <mongemmanuel@gmail.com>
2014-09-16 15:00:42 -0400

    App pruning

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(function (req, res, next){
 });
 
 app.use('/', routes);
+//No users yet, so it's commented out for now.
 //app.use('/users', users);
 
 /// catch 404 and forward to error handler
